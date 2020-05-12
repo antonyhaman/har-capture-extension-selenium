c3b600fe9c92ac5dee26392976a31dd413690ca4
kotvertolet <bytesurfer5005@gmail.com>
2020-05-12 23:20:13 +0300

    har attaching reworked, added a little bit of flexibility

--- src/main/java/com/github/kotvertolet/har/collector/HarCollectorExtension.java
+++ src/main/java/com/github/kotvertolet/har/collector/HarCollectorExtension.java
@@ -4,6 +4,7 @@ import com.browserup.bup.BrowserUpProxyServer;
 import com.browserup.bup.client.ClientUtil;
 import com.browserup.bup.proxy.CaptureType;
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.TmsLink;
 import org.junit.jupiter.api.extension.*;
@@ -11,10 +12,7 @@ import org.openqa.selenium.Proxy;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.atomic.AtomicReference;
@@ -22,66 +20,60 @@ import java.util.concurrent.atomic.AtomicReference;
 public class HarCollectorExtension implements BeforeAllCallback, BeforeTestExecutionCallback, AfterTestExecutionCallback, AfterAllCallback {
 
     private BrowserUpProxyServer proxy;
-    private String caseName;
-    private File outputFile;
     private ByteArrayOutputStream stream;
+    private static DesiredCapabilities customCapabilities;
+
+    private HarCollectorExtension() {}
+
+    public static HarCollectorExtensionBuilder builder() {
+        return new HarCollectorExtensionBuilder();
+    }
 
     @Override
     public void beforeAll(ExtensionContext extensionContext) {
         proxy = new BrowserUpProxyServer();
-        proxy.start(0);
+        proxy.start();
         proxy.enableHarCaptureTypes(CaptureType.REQUEST_BINARY_CONTENT, CaptureType.RESPONSE_BINARY_CONTENT);
         Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+
+        seleniumProxy.setHttpProxy("localhost:" + proxy.getPort());
+        seleniumProxy.setSslProxy("localhost:" + proxy.getPort());
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
+        //TODO: Check if this necessary
+        if (customCapabilities != null) {
+            capabilities.asMap().putAll(customCapabilities.asMap());
+        }
         Configuration.browserCapabilities = capabilities;
     }
 
     @Override
     public void beforeTestExecution(ExtensionContext extensionContext) {
-        AtomicReference<String> testCase = new AtomicReference<>("[NO TEST CASE]");
-        extensionContext.getElement().ifPresent(e -> {
-            if (e.getAnnotation(TmsLink.class) != null)
-                testCase.set(String.format("[%s]", e.getAnnotation(TmsLink.class).value()));
-        });
-        caseName = String.format("%s - %s", testCase.get(), extensionContext.getDisplayName());
-        proxy.newHar(caseName);
+        stream = new ByteArrayOutputStream();
+        proxy.newHar();
     }
 
     @Override
     public void afterTestExecution(ExtensionContext extensionContext) throws IOException {
-        String status = extensionContext.getExecutionException().isPresent() ? "[FAILED]" : "[PASSED]";
-        outputFile = new File(status + " - " + caseName + ".har");
-        outputFile.createNewFile();
-        proxy.getHar().writeTo(outputFile);
-
-        //proxy.getHar().writeTo(outputFile);
-    }
-
-    @Attachment(value = "HAR archive", type = "har")
-    private byte[] createAttachment(Path path) {
-        byte[] result;
-        if (path == null) {
-            result = "Cannot attach file".getBytes();
-        } else {
-            try {
-                result = Files.readAllBytes(path);
-            } catch (IOException e) {
-                result = e.getMessage().getBytes();
-            }
-        }
-        return result;
-    }
-
-    public static byte[] serialize(Object obj) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ObjectOutputStream os = new ObjectOutputStream(out);
-        os.writeObject(obj);
-        return out.toByteArray();
+        proxy.getHar().writeTo(stream);
+        Allure.addAttachment("har", new ByteArrayInputStream(stream.toByteArray()));
     }
 
     @Override
     public void afterAll(ExtensionContext extensionContext) {
         proxy.stop();
+    }
+
+    private static class HarCollectorExtensionBuilder {
+
+        public HarCollectorExtensionBuilder addDesiredCapabilities(DesiredCapabilities desiredCapabilities) {
+            customCapabilities = desiredCapabilities;
+            return this;
+        }
+
+        public HarCollectorExtension build() {
+            return new HarCollectorExtension();
+        }
     }
 }
